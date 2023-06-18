2ab479d2bb8e36319cb25100f09ffd594c71ccd3
Arthur Candido <oarthurcandido@gmail.com>
2023-06-18 15:10:46 -0300

    :sparkles: feat: input radio categoria e categoria no todo

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,18 +1,94 @@
+"use client";
 import React from "react";
-import { FiSend } from "react-icons/fi";
 import { RiAddFill } from "react-icons/ri";
+import { useState } from "react";
 
 function TodoInput() {
+  const [todo, setTodo] = useState("");
+  const [category, setCategory] = useState("leisure");
+  const [seted, setSeted] = useState({
+    work: false,
+    study: false,
+    personal: false,
+    leisure: true,
+  });
+
+  const handleSeted = (e) => {
+    setCategory(e.target.id);
+    const newSeted = {
+      work: false,
+      study: false,
+      personal: false,
+      leisure: false,
+    };
+    newSeted[e.target.id] = true;
+    setSeted(newSeted);
+  };
+
   return (
-    <div className="flex mt-2">
-      <input
-        className="w-full rounded-md dark:bg-black border border-gray-500 dark:text-gray-200"
-        type="text"
-        placeholder="O que você vai fazer hoje?"
-      />
-      <button className="hover:bg-nextGreen rounded-full ml-1 p-2">
-        <RiAddFill className="text-2xl" />
-      </button>
+    <div>
+      <div className="flex mt-2">
+        <input
+          className="w-full rounded-md dark:bg-black border border-gray-500 dark:text-gray-200"
+          type="text"
+          placeholder="O que você vai fazer hoje?"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button className="hover:bg-nextGreen rounded-full ml-1 p-2">
+          <RiAddFill className="text-2xl" />
+        </button>
+      </div>
+      <div className="flex justify-between mt-2">
+        <div>
+          <input
+            id="work"
+            type="radio"
+            className={`${
+              seted.work ? "border-2 " : ""
+            } appearance-none w-3 h-3 bg-nextGreen rounded-full mr-2`}
+            onChange={(e) => handleSeted(e)}
+            name="category"
+          />
+          <span>Trabalho</span>
+        </div>
+        <div>
+          <input
+            id="study"
+            className={`${
+              seted.study ? "border-2 " : ""
+            } appearance-none w-3 h-3 bg-nextred  rounded-full mr-2`}
+            type="radio"
+            name="category"
+            onChange={(e) => handleSeted(e)}
+          />
+          <span>Estudo</span>
+        </div>
+        <div>
+          <input
+            id="personal"
+            className={`${
+              seted.personal ? "border-2 " : ""
+            } appearance-none w-3 h-3 bg-nextBlue rounded-full mr-2`}
+            type="radio"
+            name="category"
+            onChange={(e) => handleSeted(e)}
+          />
+          <span>Pessoal</span>
+        </div>
+        <div>
+          <input
+            id="leisure"
+            className={`${
+              seted.leisure ? "border-2" : ""
+            } appearance-none w-3 h-3 bg-nextYellow rounded-full mr-2`}
+            type="radio"
+            name="category"
+            onChange={(e) => handleSeted(e)}
+          />
+          <span>Lazer</span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,34 +6,53 @@ import { RiEditLine } from "react-icons/ri";
 
 function TodoList() {
   const [checked, setChecked] = useState(false);
-  return (
-    <div
-      id="todoBox"
-      className="mt-2 p-2 border w-full flex items-center rounded-md border-gray-500 justify-between"
-    >
-      <div className="flex items-center mr-4 ">
-        <input
-          checked={checked}
-          id="green-checkbox"
-          type="checkbox"
-          value=""
-          className={` ${
-            checked ? "bg-nextGreen" : ""
-          } rounded-full border  w-4 h-4 appearance-none `}
-          onChange={() => setChecked(!checked)}
-        />
+  const [todos, setTodos] = useState(["todo1", "todo2", "todo3", "todo4"]);
 
-        <span>Todo</span>
-      </div>
-      <div>
-        <button className="hover:bg-nextred rounded-full p-2">
-          <BsTrash className="text-2xl" />
-        </button>
-        <button className="hover:bg-nextBlue rounded-full p-2 ml-2">
-          <RiEditLine className="text-2xl  " />
-        </button>
-      </div>
-    </div>
+  const category = {
+    todo1: "nextGreen",
+    todo2: "nextred",
+    todo3: "nextBlue",
+    todo4: "nextYellow",
+  };
+
+  return (
+    <>
+      {todos.map((todo, index) => (
+        <div
+          key={todo}
+          id="todoBox"
+          className="mt-2 p-2 border w-full flex items-center rounded-md border-gray-500 justify-between"
+        >
+          <div className="flex items-center mr-4 ">
+            <input
+              checked={checked}
+              id="green-checkbox"
+              type="checkbox"
+              value=""
+              className={` ${
+                checked ? "bg-nextGreen" : ""
+              } rounded-full border  w-4 h-4 appearance-none `}
+              onChange={() => setChecked(!checked)}
+            />
+
+            <span className="ml-2">{todo}</span>
+          </div>
+          <div className="flex items-center">
+            <span
+              className={`mr-2 bg-${category[todo]} rounded-full w-3 h-3`}
+            ></span>
+            <div>
+              <button className="hover:bg-nextred rounded-full p-2">
+                <BsTrash className="text-2xl" />
+              </button>
+              <button className="hover:bg-nextBlue rounded-full p-2 ml-2">
+                <RiEditLine className="text-2xl  " />
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
 
