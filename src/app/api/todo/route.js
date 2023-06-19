9a1ac40d5355c988306ec17544f4aadf55f42eea
Arthur Candido <oarthurcandido@gmail.com>
2023-06-18 21:00:22 -0300

    :sparkles: feat: rotas PATCH, DELETE e ajustes finais

--- src/app/api/todo/route.js
+++ src/app/api/todo/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { data } from "autoprefixer";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
@@ -37,4 +38,39 @@ export async function POST(req) {
   });
 
   return NextResponse.json({ message: "Criado com sucesso", todo });
+}
+
+export async function DELETE(req) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json("Não autorizado");
+  }
+
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+  const deleteTodo = await prisma.todo.delete({
+    where: {
+      id: id,
+    },
+  });
+  return NextResponse.json({ message: "Deletado com sucesso", deleteTodo });
+}
+
+export async function PATCH(req) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json("Não autorizado");
+  }
+  const data = await req.json();
+  const updateTodo = await prisma.todo.update({
+    where: {
+      id: data.id,
+    },
+    data: {
+      title: data.title,
+      completed: data.completed,
+      category: data.category,
+    },
+  });
+  return NextResponse.json({ message: "Atualizado com sucesso", updateTodo });
 }
