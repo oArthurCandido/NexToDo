79ae533e88e52a3af2377dbb2b93e1b15c72c058
Arthur Candido <oarthurcandido@gmail.com>
2023-06-18 21:39:55 -0300

    :bug: fix: prisma is not defined

--- src/app/api/todo/route.js
+++ src/app/api/todo/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { data } from "autoprefixer";
+import prisma from "@/lib/prisma";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
